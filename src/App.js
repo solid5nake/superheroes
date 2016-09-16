a93cf7e304b10c906796e0509cc96babab9b3738
solid5nake <engelenburg1980@gmail.com>
2016-09-16 10:05:10 +0200

    Adding the redux store  to our app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router';
 
 import App from './components/App';
 import Index from './components/Index';
 import About from './components/About';
+
+import * as reducers from './reducers';
+
+const reducer = combineReducers(reducers)
+
+let store = createStore(reducer, window.devToolsExtension && window.devToolsExtension());
 
 render((
   <div>
